c12f595b0dcad2069b89080f0015fe98b57d1daf
Owen Barton <owen@owenbarton.com>
2022-12-09 16:50:51 -0800

    Switch to new GCS plugin repo, add CSP config and update config format.

--- v3/config/env/production/plugins.js
+++ v3/config/env/production/plugins.js
@@ -1,24 +1,28 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'google-cloud-storage',
-    providerOptions: {
-      serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
-      bucketName: env('GCS_BUCKET_NAME', 'ca-it-prod-home-5918-strapi'),
-      basePath: env('GCS_BASE_PATH', 'production'),
-      baseUrl: env('GCS_BASE_URL', 'https://storage.googleapis.com/ca-it-prod-home-5918-strapi'),
-      publicFiles: env('GCS_PUBLIC_FILES', true),
-      uniform: env('GCS_UNIFORM', false),
+    config: {
+      provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+      providerOptions: {
+        serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
+        bucketName: env('GCS_BUCKET_NAME', 'ca-it-prod-home-5918-strapi'),
+        basePath: env('GCS_BASE_PATH', 'production'),
+        baseUrl: env('GCS_BASE_URL', 'https://storage.googleapis.com/ca-it-prod-home-5918-strapi'),
+        publicFiles: env('GCS_PUBLIC_FILES', true),
+        uniform: env('GCS_UNIFORM', false),
+      },
     },
   },
   email: {
-    provider: 'mailgun',
-    providerOptions: {
-      apiKey: env('MAILGUN_API_KEY'),
-      domain: env('MAILGUN_DOMAIN', 'mg.civicactions.net'),
-    },
-    settings: {
-      defaultFrom: 'strapi@mg.civicactions.net',
-      defaultReplyTo: 'strapi@mg.civicactions.net',
+    config: {
+      provider: 'mailgun',
+      providerOptions: {
+        key: env('MAILGUN_API_KEY'),
+        domain: env('MAILGUN_DOMAIN', 'mg.civicactions.net'),
+      },
+      settings: {
+        defaultFrom: 'strapi@mg.civicactions.net',
+        defaultReplyTo: 'strapi@mg.civicactions.net',
+      },
     },
   },
 });
