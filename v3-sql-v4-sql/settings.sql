7fe45c4fc42b9605465bfcd3b2e1fd5ffbb701d3
Owen Barton <owen@owenbarton.com>
2022-12-15 10:44:19 -0800

    Set correct config key!.

--- v3-sql-v4-sql/settings.sql
+++ v3-sql-v4-sql/settings.sql
@@ -6,9 +6,9 @@
 --
 INSERT INTO public.strapi_core_store_settings (key, value, type, environment, tag)
 VALUES (
-        'plugin_content_manager_configuration_content_types::plugin::i18n.locale',
-        '{"uid":"plugin::i18n.locale","settings":{"bulkable":true,"filterable":true,"searchable":true,"pageSize":10,"mainField":"name","defaultSortBy":"name","defaultSortOrder":"ASC"},"metadatas":{"id":{"edit":{},"list":{"label":"id","searchable":true,"sortable":true}},"name":{"edit":{"label":"name","description":"","placeholder":"","visible":true,"editable":true},"list":{"label":"name","searchable":true,"sortable":true}},"code":{"edit":{"label":"code","description":"","placeholder":"","visible":true,"editable":true},"list":{"label":"code","searchable":true,"sortable":true}},"createdAt":{"edit":{"label":"createdAt","description":"","placeholder":"","visible":false,"editable":true},"list":{"label":"createdAt","searchable":true,"sortable":true}},"updatedAt":{"edit":{"label":"updatedAt","description":"","placeholder":"","visible":false,"editable":true},"list":{"label":"updatedAt","searchable":true,"sortable":true}}},"layouts":{"list":["id","name","code","createdAt"],"edit":[[{"name":"name","size":6},{"name":"code","size":6}]]}}',
-        'object',
+        'plugin_i18n_default_locale',
+        '"en"',
+        'string',
         NULL,
         NULL
     );
