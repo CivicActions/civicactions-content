6d6f7ec100aebbc6148bb9d21b15e3810ae44498
Owen Barton <owen@owenbarton.com>
2023-06-20 16:56:58 -0700

    Auto-import API key for Gatsby.

--- v3-sql-v4-sql/migrate-schema.sh
+++ v3-sql-v4-sql/migrate-schema.sh
@@ -3,6 +3,6 @@
 # Primary migration.
 yarn start
 
-# Most migration steps.
+# Post migration steps.
 export PGPASSWORD="${DATABASE_PASSWORD}"
-psql -h "${DATABASE_HOST}" -U strapi -d strapi4 < /opt/app/settings.sql
+sed -e "s/V4_MIGRATE_API_VALUE/$V4_MIGRATE_API_VALUE/" /opt/app/settings.sql | psql -h "${DATABASE_HOST}" -U strapi -d strapi4
