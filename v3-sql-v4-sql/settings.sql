beb0397b0d429f5cc429e1a453d500a3fb5f0aa9
Owen Barton <owen@owenbarton.com>
2023-10-17 17:33:01 -0700

    Fix plugin config and add DB migration.

--- v3-sql-v4-sql/settings.sql
+++ v3-sql-v4-sql/settings.sql
@@ -20,6 +20,12 @@ VALUES (
 INSERT INTO public.strapi_api_tokens (id, name, description, type, access_key, last_used_at, expires_at, lifespan, created_at, updated_at, created_by_id, updated_by_id) VALUES (1, 'Gatsby', '', 'full-access', 'V4_MIGRATE_API_VALUE', NULL, NULL, NULL, '2023-06-20 23:17:32.278', '2023-06-20 23:17:32.278', NULL, NULL);
 
 --
+-- From https://github.com/strapi-community/strapi-provider-upload-google-cloud-storage/blob/master/MIGRATION_GUIDE.md
+--
+
+UPDATE public.files SET provider = 'strapi-provider-upload-google-cloud-storage'  WHERE provider = 'google-cloud-storage';
+
+--
 -- PostgreSQL database dump complete
 --
 
